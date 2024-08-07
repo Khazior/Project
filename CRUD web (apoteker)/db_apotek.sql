e67143daf6e51ebd1624afb472b44243b0a53650
Gibran Maulana <122756881+Khazior@users.noreply.github.com>
2024-08-07 21:36:39 +0800

    BOOTSTRAP, PHP & MYSQL

---
+++ CRUD web (apoteker)/db_apotek.sql
@@ -0,0 +1,215 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 26, 2024 at 04:58 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_apotek`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `akun_admin`
+--
+
+CREATE TABLE `akun_admin` (
+  `id_admin` int(11) NOT NULL,
+  `email` varchar(75) NOT NULL,
+  `password` varchar(40) NOT NULL,
+  `level` varchar(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `akun_admin`
+--
+
+INSERT INTO `akun_admin` (`id_admin`, `email`, `password`, `level`) VALUES
+(1, 'admin@gmail.com', 'f865b53623b121fd34ee5426c792e5c33af8c227', 'Y');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kategori`
+--
+
+CREATE TABLE `kategori` (
+  `id_kategori` int(5) NOT NULL,
+  `nama_kategori` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `kategori`
+--
+
+INSERT INTO `kategori` (`id_kategori`, `nama_kategori`) VALUES
+(1, 'obat bebas'),
+(2, 'obat bebas terbatas');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `obat`
+--
+
+CREATE TABLE `obat` (
+  `id_obat` int(11) NOT NULL,
+  `nama_obat` varchar(100) NOT NULL,
+  `harga_obat` int(11) NOT NULL,
+  `stok_obat` int(11) NOT NULL,
+  `id_kategori` int(5) NOT NULL,
+  `khasiat` varchar(100) NOT NULL,
+  `expired` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `obat`
+--
+
+INSERT INTO `obat` (`id_obat`, `nama_obat`, `harga_obat`, `stok_obat`, `id_kategori`, `khasiat`, `expired`) VALUES
+(1, 'bodrex', 12500, 95, 1, 'meredakan sakit kepala dan pusing', '2027-07-24'),
+(2, 'paracetamol', 13000, 100, 1, 'meredakan demam panas pada anak', '2027-07-24'),
+(3, 'promagh', 9300, 100, 2, 'mengurangi sakit perut akibat belum sarapan', '2027-07-24');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pelanggan`
+--
+
+CREATE TABLE `pelanggan` (
+  `id_pelanggan` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `pelanggan`
+--
+
+INSERT INTO `pelanggan` (`id_pelanggan`, `username`, `password`) VALUES
+(100, 'kaziro', 'admin'),
+(200, 'gibran', 'admin'),
+(300, 'husen', 'admin');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `penjualan`
+--
+
+CREATE TABLE `penjualan` (
+  `id_penjualan` int(11) NOT NULL,
+  `id_obat` int(11) NOT NULL,
+  `id_pelanggan` int(11) NOT NULL,
+  `tanggal_terjual` date NOT NULL,
+  `jumlah_obat` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `penjualan`
+--
+
+INSERT INTO `penjualan` (`id_penjualan`, `id_obat`, `id_pelanggan`, `tanggal_terjual`, `jumlah_obat`) VALUES
+(1, 1, 100, '2024-07-25', 5);
+
+--
+-- Triggers `penjualan`
+--
+DELIMITER $$
+CREATE TRIGGER `pembelian_obat` AFTER INSERT ON `penjualan` FOR EACH ROW BEGIN
+    UPDATE obat
+    SET stok_obat = stok_obat - NEW.jumlah_obat
+    WHERE id_obat = NEW.id_obat;
+END
+$$
+DELIMITER ;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `akun_admin`
+--
+ALTER TABLE `akun_admin`
+  ADD PRIMARY KEY (`id_admin`);
+
+--
+-- Indexes for table `kategori`
+--
+ALTER TABLE `kategori`
+  ADD PRIMARY KEY (`id_kategori`);
+
+--
+-- Indexes for table `obat`
+--
+ALTER TABLE `obat`
+  ADD PRIMARY KEY (`id_obat`),
+  ADD KEY `id_kategori` (`id_kategori`);
+
+--
+-- Indexes for table `pelanggan`
+--
+ALTER TABLE `pelanggan`
+  ADD PRIMARY KEY (`id_pelanggan`);
+
+--
+-- Indexes for table `penjualan`
+--
+ALTER TABLE `penjualan`
+  ADD PRIMARY KEY (`id_penjualan`),
+  ADD KEY `id_obat` (`id_obat`),
+  ADD KEY `id_pelanggan` (`id_pelanggan`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `akun_admin`
+--
+ALTER TABLE `akun_admin`
+  MODIFY `id_admin` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `penjualan`
+--
+ALTER TABLE `penjualan`
+  MODIFY `id_penjualan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `obat`
+--
+ALTER TABLE `obat`
+  ADD CONSTRAINT `obat_ibfk_1` FOREIGN KEY (`id_kategori`) REFERENCES `kategori` (`id_kategori`);
+
+--
+-- Constraints for table `penjualan`
+--
+ALTER TABLE `penjualan`
+  ADD CONSTRAINT `penjualan_ibfk_1` FOREIGN KEY (`id_obat`) REFERENCES `obat` (`id_obat`),
+  ADD CONSTRAINT `penjualan_ibfk_2` FOREIGN KEY (`id_pelanggan`) REFERENCES `pelanggan` (`id_pelanggan`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
